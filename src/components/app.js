548a7d308ffa5f47801d498249d366abccafa5c9
nathenjo <nateselectronicrepair@gmail.com>
2022-01-11 22:56:31 -0500

    Finished navbar and image mobile responsiveness and updated styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import YellowWoodLogo from '../../static/assets/logos/drive-download-20220111T233455Z-001/yelow-wood.jpg';
+import PngLogo from '../../static/assets/logos/drive-download-20220111T233455Z-001/1.png';
 import NavBar from './navbar';
 
 export default function App() {
   const [screenSize, setScreenSize] = useState(window.innerWidth);
-  const [imageWidth, setImageWidth] = useState(400);
+  const [imageWidth, setImageWidth] = useState(600);
 
   useEffect(() => {
     window.addEventListener('resize', handleScreenSize)
@@ -19,12 +19,12 @@ export default function App() {
     if (screenSize < 600) {
       setImageWidth(screenSize)
     } else {
-      setImageWidth(400)
+      setImageWidth(600)
     }
   }
     return (
       <div className='app'>
-        <img className='app__header-logo' src={YellowWoodLogo} width={imageWidth} />
+        <img className='app__header-logo' src={PngLogo} width={imageWidth} />
         <NavBar />
       </div>
     );
